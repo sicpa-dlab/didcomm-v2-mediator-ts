f4d066fbc0b37202653e4b900e8d829a2725feaa
artem.ivanov <artem.ivanov@dsr-corporation.com>
2022-08-31 15:06:42 +0300

    Use different package for getting version (#157)
    * Added mattermost notifications on service start
    
    * Fixed lint errors
    
    * Changed env variable name
    
    * Always use pretty logger
    
    * Updated AFJ version
    
    * Send notification if endpoint specified
    
    * Fixed prettier errors
    
    * Use different package for getting version
    
    * Fixed prettier errors

--- src/common/utils/mattermost.ts
+++ src/common/utils/mattermost.ts
@@ -1,17 +1,17 @@
 import ExpressConfig from '@config/express'
-import * as nbgv from 'nerdbank-gitversioning'
 /* tslint:disable no-var-requires */
 const Mattermost = require('node-mattermost')
+/* tslint:disable no-var-requires */
+const getVersion = require('git-repo-version')
 
 export const sendNotification = async () => {
   const expressConfig = ExpressConfig()
-  const versionInfo = await nbgv.getVersion()
 
   if (!expressConfig.notificationsEndpoint) return
 
   const mattermost = new Mattermost(expressConfig.notificationsEndpoint)
   await mattermost.send({
     username: 'Bot',
-    text: `Application ${expressConfig.name}-${versionInfo.npmPackageVersion} has been started.`,
+    text: `Application ${expressConfig.name}-${getVersion()} has been started.`,
   })
 }
